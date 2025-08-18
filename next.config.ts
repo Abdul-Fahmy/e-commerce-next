6c050526db3fb06042d11d9c6f5c2292500304a1
Abdulrhman Fahmy <abdelrhmanfahmy69@gmail.com>
2025-08-19 02:02:17 +0300

    fix barnds image config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    
     remotePatterns: [
       {
         protocol: "https",
@@ -14,6 +13,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "ecommerce.routemisr.com",
         pathname: "/Route-Academy-categories/*",
+      },
+      {
+        protocol: "https",
+        hostname: "ecommerce.routemisr.com",
+        pathname: "/Route-Academy-brands/*",
       },
     ],
   },
