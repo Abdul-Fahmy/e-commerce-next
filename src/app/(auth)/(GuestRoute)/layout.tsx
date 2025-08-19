df38fd8c26c5b245fea3a1f5354719976ebc16f8
Abdulrhman Fahmy <abdelrhmanfahmy69@gmail.com>
2025-08-19 20:24:07 +0300

    remove guest and protected

--- src/app/(auth)/(GuestRoute)/layout.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import GuestRoute from "@/components/GuestRoute/GuestRoute";
-import { ReactNode } from "react";
-
-export default function GuestLayout({ children }: { children: ReactNode }) {
-  return <GuestRoute>{children}</GuestRoute>;
-}
