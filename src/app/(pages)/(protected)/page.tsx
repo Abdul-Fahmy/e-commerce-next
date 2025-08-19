cc549ff1b6b90491da6ad912f96fdf2931789f0c
Abdulrhman Fahmy <abdelrhmanfahmy69@gmail.com>
2025-08-19 19:57:15 +0300

    fix

--- src/app/(pages)/(protected)/page.tsx
+++ src/app/(pages)/(protected)/page.tsx
@@ -1,17 +1,15 @@
-"use client";
 import HomeSlider from "@/components/HomeSlider/HomeSlider";
 import Products from "./product/page";
 import CategorySlider from "@/components/CategorySlider/CategorySlider";
-import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
 
 export default function page() {
   return (
     <>
-      <ProtectedRoute>
+      
         <HomeSlider />
         <CategorySlider />
         <Products />
-      </ProtectedRoute>
+     
     </>
   );
 }
