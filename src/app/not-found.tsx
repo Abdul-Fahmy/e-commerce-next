446f6e46c25801bc18f587cc9581562eb2b3e091
Abdulrhman Fahmy <abdelrhmanfahmy69@gmail.com>
2025-08-20 01:58:58 +0300

    adding mail to 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,31 @@
-import styles from './notFound.module.scss';
-import Link from 'next/link';
+import styles from "./notFound.module.scss";
+import Link from "next/link";
 
 export default function NotFound() {
-  return (
-    <main className={`${styles.bsod} ${styles.container}`}>
-      <h1 className={`${styles.neg} ${styles.title}`}>
-        <span className={styles.bg}>Error - 404</span>
-      </h1>
+    return (
+        <main className={`${styles.bsod} ${styles.container}`}>
+            <h1 className={`${styles.neg} ${styles.title}`}>
+                <span className={styles.bg}>Error - 404</span>
+            </h1>
 
-      <p>An error has occurred, to continue:</p>
-      <p>
-        * Return to our homepage.<br />
-        * Send us an e-mail about this error and try later.
-      </p>
+            <p>An error has occurred, to continue:</p>
+            <p>
+                * Return to our homepage.
+                <br />* Send us an e-mail about this error and try later.
+            </p>
 
-      <nav className={styles.nav}>
-        <Link href="/" className={styles.link}>index</Link>
-        &nbsp;|&nbsp;
-        <a href="mailto:webmaster@example.com" className={styles.link}>webmaster</a>
-      </nav>
-    </main>
-  );
+            <nav className={styles.nav}>
+                <Link href="/" className={styles.link}>
+                    index
+                </Link>
+                &nbsp;|&nbsp;
+                <a
+                    href="mailto:abdelrhmanfahmy69@gmail.com"
+                    className={styles.link}
+                >
+                    webmaster
+                </a>
+            </nav>
+        </main>
+    );
 }
